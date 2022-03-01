f5c8643c19250aa000a4cf69a1ca02f7ca87212d
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-03-01 20:22:10 +0330

    week3 E03 add audioPlayers package and useit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(Application());
@@ -26,11 +27,49 @@ Widget getApplication() {
               fit: BoxFit.fitWidth,
             ),
           ),
-          child: Center(
-            child: Text('AmirahmadAdibi'),
-          ),
+          child: getBody(),
         ),
       ),
     ),
+  );
+}
+
+Widget getBody() {
+  return Column(
+    children: [
+      Expanded(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+                child: TextButton(
+                    onPressed: () {
+                      var player = AudioCache();
+                      player.play('h1.wav');
+                    },
+                    child: Text(''))),
+            Expanded(child: TextButton(onPressed: () {}, child: Text('')))
+          ],
+        ),
+      ),
+      Expanded(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(child: TextButton(onPressed: () {}, child: Text(''))),
+            Expanded(child: TextButton(onPressed: () {}, child: Text('')))
+          ],
+        ),
+      ),
+      Expanded(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(child: TextButton(onPressed: () {}, child: Text(''))),
+            Expanded(child: TextButton(onPressed: () {}, child: Text('')))
+          ],
+        ),
+      )
+    ],
   );
 }
