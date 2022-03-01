e8dcf4a26fc3d7d5433cb4f2e0167895fffb62c0
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-03-01 19:21:10 +0330

    week4 E01 decoratedBxo and background image

---
+++ android/app/src/main/kotlin/com/example/drum_x/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.drum_x
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return getApplication();
+  }
+}
+
+Widget getApplication() {
+  return MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.amber,
+      body: SafeArea(
+        child: DecoratedBox(
+          position: DecorationPosition.background,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/drum.png'),
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          child: Center(
+            child: Text('AmirahmadAdibi'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
